cd6592dba7c74c7fabf139591650066040cfc12f
Alex Mayfield <alexmax2742@gmail.com>
2018-09-25 19:05:18 -0400

    Apparently gdb >= 8 fixes the thread naming issue

--- demo/sdl_opengl3/main.nim
+++ demo/sdl_opengl3/main.nim
@@ -71,8 +71,9 @@ var bg: nuklear.colorf
 ##  SDL setup
 discard sdl2.setHint("SDL_VIDEO_HIGHDPI_DISABLED", "0")
 when not defined(vcc):
-  ## [AM] Needed to ensure that gdb doesn't get confused by SDL's attempts
-  ##      at renaming threads on Windows using RaiseException.
+  ## [AM] Needed to ensure that older versions of gdb don't get confused
+  ##      by SDL's attempts at renaming threads on Windows using RaiseException.
+  ##      I think this hint can be disabled as of gdb 8.0, but I'm not sure.
   discard sdl2.setHint("SDL_WINDOWS_DISABLE_THREAD_NAMING", "1")
 sdl2.init(sdl2.INIT_VIDEO or sdl2.INIT_TIMER or sdl2.INIT_EVENTS)
 discard sdl2.glSetAttribute(SDL_GL_CONTEXT_FLAGS,
